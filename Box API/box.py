8a46a6ee8ccce5d34e5a0bf870736f60c94c6c3b
Abhishek Karimpuzha Devarajan <66976767+ABeeShake@users.noreply.github.com>
2021-07-06 09:16:46 -0700

    updated docstring and if statement

--- Box API/box.py
+++ Box API/box.py
@@ -3,12 +3,7 @@ from boxsdk import DevelopmentClient
 
 def box_upload(csv_file):
     '''
-    Notes: Create a text file named app.cfg in pycharm. Add the following info in order:
-            1. Client ID
-            2. Client Secret
-            3. Developer Token
-
-            On the Box API site, go to your app and change settings:
+    Notes:  On the Box API site, go to your app and change settings:
             1. read and write all files and folders to Box - should be activated
             2. Manage users and manage groups - should be activated
             3. Make API calls using as-user header - should be activated
@@ -28,9 +23,6 @@ def box_upload(csv_file):
     print(f'File {new_file.name} was uploaded to {folder.name}')
 
 if __name__ == '__main__':
-    with open('./app.cfg', 'r') as app_cfg:
-        client_id = app_cfg.readline()
-        client_secret = app_cfg.readline()
-        access_token = app_cfg.readline()
 
-    print(access_token)
+    pass
+
